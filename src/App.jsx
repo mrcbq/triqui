b82d56f0a6463cd40f5374c04b9baa6dfeffecb4
mrcbq <marcobaquero@gmail.com>
2023-12-08 23:06:09 -0500

    function setWinner completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,90 @@
 import { useState } from 'react';
-import './index.css'
+import './index.css';
 const TURNS = {
   X: 'x',
-  O: 'o'
-}
+  O: 'o',
+};
 
 const initialBoard = Array(9).fill(null);
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
 
   const handleClick = () => {
-    updateBoard(index)
-  }
+    updateBoard(index);
+  };
   return (
     <div onClick={handleClick} className={className}>
       {children}
     </div>
-  )
-}
+  );
+};
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
 function App() {
-  const [board, setBoard] = useState(initialBoard)
+  const [board, setBoard] = useState(initialBoard);
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const [winner, setWinner] = useState(null); //null means no winner, false means draw
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
 
   const updateBoard = (index) => {
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
-  }
+    if (board[index] || winner) return;
 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
-        {
-          board.map((_, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {board[index]}
-              </Square>
-            )
-          })
-        }
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
       </section>
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
